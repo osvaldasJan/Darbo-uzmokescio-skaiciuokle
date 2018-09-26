aae0cacaa830adf21244e9506b4b053a9e03c28b
osvaldasJan <jankauskas.osvaldas@gmail.com>
2018-09-26 11:03:06 +0300

    --

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
                 * Vartotojas įveda bazinį darbuotojo atlyginimą ir pagamintų detalių kiekį;
                 norma- 2000 vnt.
                 * Sukurti funkciją, skaičiuojančią darbuotojo atlyginimą
-                (atlyginimas * koeficientas);
+                (DarboUzmokestis * koeficientas);
                 * Per parametrus funkcijai perduoti bazinį darbuotojo atlyginimą ir pagamintų detalių kiekį;
                 * JEIGU Nepadarė normos (<2000): 0.5 kof
                 Pasiekė normą (2000 - 2500): 1 kof
                 Viršijo normą (>2500): 1.5 kof
                 * PVZ.:
-                atlyginimas = 1000;
+                DarboUzmokestis = 1000;
                 detaliuKiekis = 1500;
-                Darbuotojo mėnesinis atlyginimas - 500 Eur.
+                Darbuotojo mėnesinis DarboUzmokestis - 500 Eur.
 */
 
 public class Main {
@@ -21,17 +21,26 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Įveskite vartotojo bazinį atlyginimą: ");
-        int bazinisAtlyginimas = keyboard.nextInt();
-        System.out.println("Įveskite pagamintų detalių skaičių: ");
-        int pagamintosDetales = keyboard.nextInt();
+        int bazinisDarboUzmokestis;
+        int pagamintosDetales;
 
-        System.out.println("Galutinis atlyginimas, kai pagamintos " +
-                pagamintosDetales + " detalės: " +
-                atlyginimoSkaiciavimas (bazinisAtlyginimas,
-                koeficientoSkaiciavimas(pagamintosDetales)));
+        System.out.println("Darbo užmokesčio skaičiavimas");
+        /*System.out.println("Įveskite darbuotojo bazinį atlyginimą: ");
+        bazinisDarboUzmokestis = keyboard.nextInt();
+        System.out.println("Įveskite darbuotojo pagamintų detalių skaičių: ");
+        pagamintosDetales = keyboard.nextInt();
+        */
+        /*System.out.println("Galutinis atlyginimas: " +
+                apskaiciuotiAtlyginima());
+        */
+        /*System.out.println("Galutinis atlyginimas, kai bazinis darbo užmokestis " + bazinisDarboUzmokestis +
+                " ir pagamintos " + pagamintosDetales + " detalės: " +
+                apskaiciuotiAtlyginima(bazinisDarboUzmokestis,
+                apskaiciuotiKoeficienta(pagamintosDetales)));
+                */
+        apskaiciuotiAtlyginima2();
     }
-    public static double koeficientoSkaiciavimas (int detaliuKiekis){
+    public static double apskaiciuotiKoeficienta(int detaliuKiekis){
         if (detaliuKiekis < 2000) {
             return 0.5;
         } else if (detaliuKiekis > 2500) {
@@ -39,9 +48,30 @@ public class Main {
         } else
             return 1;
     }
-    public static double atlyginimoSkaiciavimas (int bazinisAtlyginimas, double koeficientas){
-        return koeficientas*bazinisAtlyginimas;
+    public static double apskaiciuotiAtlyginima(int bazinisDarboUzmokestis, double koeficientas){ //metodas su parametrais
+        return koeficientas*bazinisDarboUzmokestis;
     }
+    public static double apskaiciuotiAtlyginima(){ //metodas be parametru
+        System.out.println("Įveskite darbuotojo bazinį atlyginimą: ");
+        int bazinisDarboUzmokestis = keyboard.nextInt();
+        System.out.println("Įveskite darbuotojo pagamintų detalių skaičių: ");
+        int pagamintosDetales = keyboard.nextInt();
+
+        return bazinisDarboUzmokestis*apskaiciuotiKoeficienta(pagamintosDetales);
+            }
+
+    public static void apskaiciuotiAtlyginima2(){
+        System.out.println("Įveskite darbuotojo bazinį atlyginimą: ");
+        int bazinisDarboUzmokestis = keyboard.nextInt();
+        System.out.println("Įveskite darbuotojo pagamintų detalių skaičių: ");
+        int pagamintosDetales = keyboard.nextInt();
+
+        System.out.println("Galutinis atlyginimas, kai bazinis darbo užmokestis " + bazinisDarboUzmokestis +
+                " ir pagamintos " + pagamintosDetales + " detalės: " +
+                apskaiciuotiAtlyginima(bazinisDarboUzmokestis,
+                        apskaiciuotiKoeficienta(pagamintosDetales)));
+    }
+
     public static void bark() {
         System.out.println("bark");
     }
