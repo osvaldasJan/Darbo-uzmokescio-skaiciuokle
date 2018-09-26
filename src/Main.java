b216d1d456afe12434e986f9e7c0dec3a301e1bb
osvaldasJan <jankauskas.osvaldas@gmail.com>
2018-09-26 09:09:14 +0300

    modified

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,35 @@
 import java.util.Scanner;
 
+/*Reikalavimai:
+                * Vartotojas įveda bazinį darbuotojo atlyginimą ir pagamintų detalių kiekį;
+                norma- 2000 vnt.
+                * Sukurti funkciją, skaičiuojančią darbuotojo atlyginimą
+                (atlyginimas * koeficientas);
+                * Per parametrus funkcijai perduoti bazinį darbuotojo atlyginimą ir pagamintų detalių kiekį;
+                * JEIGU Nepadarė normos (<2000): 0.5 kof
+                Pasiekė normą (2000 - 2500): 1 kof
+                Viršijo normą (>2500): 1.5 kof
+                * PVZ.:
+                atlyginimas = 1000;
+                detaliuKiekis = 1500;
+                Darbuotojo mėnesinis atlyginimas - 500 Eur.
+*/
+
 public class Main {
 
     static Scanner keyboard = new Scanner(System.in);
 
     public static void main(String[] args) {
-	// write your code here
-        /*
-        Reikalavimai:
-* Vartotojas įveda bazinį darbuotojo atlyginimą ir pagamintų detalių kiekį;
-norma- 2000 vnt.
-* Sukurti funkciją, skaičiuojančią darbuotojo atlyginimą
-(atlyginimas * koeficientas);
-* Per parametrus funkcijai perduoti bazinį darbuotojo atlyginimą ir pagamintų detalių kiekį;
-* JEIGU Nepadarė normos (<2000): 0.5 kof
-Pasiekė normą (2000 - 2500): 1 kof
-Viršijo normą (>2500): 1.5 kof
-* PVZ.:
-atlyginimas = 1000;
-detaliuKiekis = 1500;
-Darbuotojo mėnesinis atlyginimas - 500 Eur.
-        */
-        System.out.println("Įveskite vartotojo bazinį atlyginimą: ir pagamintų detalių skaičių: ");
-        System.out.println("Galutinis atlyginimas: " + atlyginimoSkaiciavimas (keyboard.nextInt(),koeficientoSkaiciavimas(keyboard.nextInt())));
+
+        System.out.println("Įveskite vartotojo bazinį atlyginimą: ");
+        int bazinisAtlyginimas = keyboard.nextInt();
+        System.out.println("Įveskite pagamintų detalių skaičių: ");
+        int pagamintosDetales = keyboard.nextInt();
+
+        System.out.println("Galutinis atlyginimas, kai pagamintos " +
+                pagamintosDetales + " detalės: " +
+                atlyginimoSkaiciavimas (bazinisAtlyginimas,
+                koeficientoSkaiciavimas(pagamintosDetales)));
     }
     public static double koeficientoSkaiciavimas (int detaliuKiekis){
         if (detaliuKiekis < 2000) {
